09846b95f4251fa5534d693f8669fa508c33f2d8
marcusvc <marcusvcsilva@gmail.com>
2020-04-14 17:59:31 -0300

    Turn Article component into a stateless component for simplicity and avoid side effects.

--- src/component/article/Article.jsx
+++ src/component/article/Article.jsx
@@ -1,26 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Article extends Component {
-    render() {
-        return (
-            <div className="col">
-                <div className="card">
-                    <a href={this.props.article.url} target="_blank" rel="noopener noreferrer">
-                        <img className="card-img-top" src={this.props.article.urlToImage} alt={this.props.article.title} />
-                    </a>
-                    <div className="card-body">
-                        <h5 className="card-title">{this.props.article.title}</h5>
-                        <p className="card-text">
-                            {this.props.article.description}
-                        </p>
-                        <p className="card-text">
-                            <small className="text-muted">Fonte: {this.props.article.source.name}</small>
-                        </p>
-                    </div>
+function Article(props) {
+    return (
+        <div className="col">
+            <div className="card">
+                <a href={props.article.url} target="_blank" rel="noopener noreferrer">
+                    <img className="card-img-top" src={props.article.urlToImage} alt={props.article.title} />
+                </a>
+                <div className="card-body">
+                    <h5 className="card-title">{props.article.title}</h5>
+                    <p className="card-text">
+                        {props.article.description}
+                    </p>
+                    <p className="card-text">
+                        <small className="text-muted">Fonte: {props.article.source.name}</small>
+                    </p>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Article
