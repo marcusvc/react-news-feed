3647da62163d9c6a2781e18153b923e5ff387fe2
Marcus <marcusvcsilva@gmail.com>
2020-04-20 03:04:27 -0300

    Control of filter fields rendering

--- src/component/filter/Filter.jsx
+++ src/component/filter/Filter.jsx
@@ -2,9 +2,54 @@ import React, { Component } from 'react'
 import { COUNTRY, TYPE, CATEGORY, LANGUAGE } from '../../constant/Constant'
 
 class Filter extends Component {
-    /*constructor(props) {
+    constructor(props) {
         super(props)
-    }*/
+        this._queryRef = React.createRef()
+        this._HANDLE_MAP = {
+            typeSelect: this._setType,
+            country: this._setValue,
+            category: this._setValue,
+        }
+        this.state = {
+            options: {}
+        }
+        this._handleFilter = this._handleFilter.bind(this)
+    }
+    
+    _handleFilterChange = (event) => {
+        this._HANDLE_MAP[event.target.id](event.target)
+    }
+
+    _setType = (target) => {
+        let _options = {}
+        if (this._queryRef.current) {
+            this._queryRef.current.value = ''
+        }
+        if (target.value) {
+            _options[target.id] = target.value
+        }
+        this.setState({
+            options: {..._options}
+        })
+    }
+
+    _setValue = (target) => {
+        this.setState((state) => {
+            let _options = {...state.options}
+            _options[target.id] = target.value
+            return {
+                options: _options
+            }
+        })
+    }
+
+    _handleFilter = () => {
+        this.props.handleFilter({
+            ...this.state.options,
+            query: this._queryRef.current.value
+        })
+    }
+
     render() {
         return (
             <div className="app-filter">
@@ -15,19 +60,21 @@ class Filter extends Component {
                     <div className="collapse" id="filter-options">
                         <div className="input-group mb-2">
                             <div className="input-group-prepend">
-                                <label className="input-group-text" htmlFor="typeGroup">Type</label>
+                                <label className="input-group-text" htmlFor="typeSelect">Type</label>
                             </div>
-                            <select className="custom-select" id="typeGroup">
-                                <option defaultValue>Choose...</option>
+                            <select className="custom-select" id="typeSelect" onChange={this._handleFilterChange}>
+                                <option value={''}>Choose...</option>
                                 {TYPE.map((type, index) => <option key={index} value={type.value}>{type.name}</option>)}
                             </select>
                         </div>
+                        {this.state.options.typeSelect && this.state.options.typeSelect === 'top-headlines' &&
+                        <React.Fragment>
                         <div className="input-group mb-2">
                             <div className="input-group-prepend">
                                 <label className="input-group-text" htmlFor="country">Country</label>
                             </div>
-                            <select className="custom-select" id="country">
-                                <option defaultValue>Choose...</option>
+                            <select className="custom-select" id="country" onChange={this._handleFilterChange}>
+                                <option value={''}>Choose...</option>
                                 {COUNTRY.map((country, index) => <option key={index} value={country}>{country}</option>)}
                             </select>
                         </div>
@@ -35,17 +82,14 @@ class Filter extends Component {
                             <div className="input-group-prepend">
                                 <label className="input-group-text" htmlFor="category">Category</label>
                             </div>
-                            <select className="custom-select" id="category">
-                                <option defaultValue>Choose...</option>
+                            <select className="custom-select" id="category" onChange={this._handleFilterChange}>
+                                <option value={''}>Choose...</option>
                                 {CATEGORY.map((category, index) => <option key={index} value={category.value}>{category.name}</option>)}
                             </select>
                         </div>
-                        <div className="input-group mb-2">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text" id="querySpan">Query</span>
-                            </div>
-                            <input type="text" className="form-control" id="query" aria-describedby="querySpan" />
-                        </div>
+                        </React.Fragment>}
+                        {this.state.options.typeSelect && this.state.options.typeSelect === 'everything' &&
+                        <React.Fragment>
                         <div className="input-group mb-2">
                             <div className="input-group-prepend">
                                 <span className="input-group-text" id="fromDateSpan">From</span>
@@ -63,10 +107,26 @@ class Filter extends Component {
                                 <label className="input-group-text" htmlFor="language">Language</label>
                             </div>
                             <select className="custom-select" id="language">
-                                <option defaultValue>Choose...</option>
+                                <option value={''}>Choose...</option>
                                 {LANGUAGE.map((language, index) => <option key={index} value={language.value}>{language.name}</option>)}
                             </select>
                         </div>
+                        </React.Fragment>}
+                        {this.state.options.typeSelect &&
+                        <React.Fragment>
+                        <div className="input-group mb-2">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="querySpan">Query</span>
+                            </div>
+                            <input type="text" className="form-control" id="query"
+                                aria-describedby="querySpan"
+                                placeholder="(lmia OR immigration OR visa OR work) AND canada"
+                                ref={this._queryRef} />
+                        </div>
+                        <div className="input-group mb-2">
+                            <button type="button" className="btn btn-light" onClick={this._handleFilter}>Apply</button>
+                        </div>
+                        </React.Fragment>}
                     </div>
                 </div>
             </div>
