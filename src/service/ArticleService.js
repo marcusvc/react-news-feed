5e624d31a8b55e3c4e2993cf5cd8dc8639101877
marcusvc <marcusvcsilva@gmail.com>
2020-04-22 18:02:24 -0300

    Forwarding the request parameters from the client to to the internal API

--- src/service/ArticleService.js
+++ src/service/ArticleService.js
@@ -1,21 +1,13 @@
-const apiToken = 'Bearer 42b9f17d6415491581c7b393b2e6ed4c'
-const baseUrl = '//newsapi.org/v2/'
+const baseUrl = '/api/'
 
 export default (options) => {
-  const today = new Date()
-  const yesterday = new Date()
-  yesterday.setDate(today.getDate() - 1)
+  console.log(options)
   // const url = '//newsapi.org/v2/top-headlines?country=us'
   // const url = `//newsapi.org/v2/everything?q=ronaldinho&page=${this.currentPage}`
-  const url = `${baseUrl}${options.typeSelect}?q=${encodeURI(options.query)}&from=${yesterday.toISOString().substr(0, 10)}&to=${today.toISOString().substr(0, 10)}&page=${options.page}`
+  const url = `${baseUrl}${options.typeSelect}?q=${encodeURI(options.query)}&from=${options.fromDate}&to=${options.toDate}&page=${options.page}&language=${options.language}`
   const req = new Request(url)
-  const headers = new Headers({
-    Authorization: apiToken
-  })
   const params = {
-    method: 'GET',
-    mode: 'cors',
-    headers: headers
+    method: 'GET'
   }
   return fetch(req, params)
 }
