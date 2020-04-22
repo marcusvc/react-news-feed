a71b5de23d2bd9e895a4eef94ff580bb4db0879f
marcusvc <marcusvcsilva@gmail.com>
2020-04-22 03:27:47 -0300

    Undo module settings for express server

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-import express, { static } from 'express'
-import bodyParser from 'body-parser'
-import { join } from 'path'
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
 const app = express()
 
 const port = process.env.PORT || 8080
 
-app.use(static(join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/ping', function (req, res) {
 	return res.send('pong')
 })
 
 app.get('/', function (req, res) {
-	res.sendFile(join(__dirname, 'build', 'index.html'))
+	res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 app.listen(port, () => console.log(`App is listening on port ${port}`))
