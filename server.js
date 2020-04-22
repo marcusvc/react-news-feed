7264e7c3a9800bef3455892530ab096d1804e3b3
marcusvc <marcusvcsilva@gmail.com>
2020-04-22 03:00:35 -0300

    Converting to ES6 module

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
+import express, { static } from 'express'
+import bodyParser from 'body-parser'
+import { join } from 'path'
 const app = express()
 
 const port = process.env.PORT || 8080
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(static(join(__dirname, 'build')))
 
 app.get('/ping', function (req, res) {
 	return res.send('pong')
 })
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'))
+	res.sendFile(join(__dirname, 'build', 'index.html'))
 })
 
 app.listen(port, () => console.log(`App is listening on port ${port}`))
