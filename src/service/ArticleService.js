d01b3cc2377176e010c46beaf3d56081d6e16866
marcusvc <marcusvcsilva@gmail.com>
2020-04-22 18:08:19 -0300

    Cleaning up comments

--- src/service/ArticleService.js
+++ src/service/ArticleService.js
@@ -1,9 +1,6 @@
 const baseUrl = '/api/'
 
 export default (options) => {
-  console.log(options)
-  // const url = '//newsapi.org/v2/top-headlines?country=us'
-  // const url = `//newsapi.org/v2/everything?q=ronaldinho&page=${this.currentPage}`
   const url = `${baseUrl}${options.typeSelect}?q=${encodeURI(options.query)}&from=${options.fromDate}&to=${options.toDate}&page=${options.page}&language=${options.language}`
   const req = new Request(url)
   const params = {
