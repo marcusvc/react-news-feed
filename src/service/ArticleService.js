8f6da41eabf2db6a64a7c02f7b6fc4cbc13c2f5e
marcusvc <marcusvcsilva@gmail.com>
2020-04-22 02:58:05 -0300

    Removing the protocol of the URL. Using the protocol relative technic

--- src/service/ArticleService.js
+++ src/service/ArticleService.js
@@ -1,13 +1,13 @@
 const apiToken = 'Bearer 42b9f17d6415491581c7b393b2e6ed4c'
-const baseUrl = 'http://newsapi.org/v2/'
+const baseUrl = '//newsapi.org/v2/'
 
 export default (options) => {
 
     const today = new Date()
     const yesterday = new Date()
     yesterday.setDate(today.getDate() - 1)
-    //const url = 'http://newsapi.org/v2/top-headlines?country=us'
-    //const url = `http://newsapi.org/v2/everything?q=ronaldinho&page=${this.currentPage}`
+    //const url = '//newsapi.org/v2/top-headlines?country=us'
+    //const url = `//newsapi.org/v2/everything?q=ronaldinho&page=${this.currentPage}`
     const url = `${baseUrl}${options.typeSelect}?q=${encodeURI(options.query)}&from=${yesterday.toISOString().substr(0, 10)}&to=${today.toISOString().substr(0, 10)}&page=${options.page}`
     const req = new Request(url)
     const headers = new Headers({
